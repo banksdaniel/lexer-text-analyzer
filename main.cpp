11b5dd190bde49a33c8b0e401a8c106c4e772f8f
Daniel Cunha de Oliveira <daniel.oliveira@coopersystem.com.br>
2022-12-05 15:35:49 -0300

    ⚡️ Introducing 'dicionario' variable

--- main.cpp
+++ main.cpp
@@ -14,3 +14,31 @@ static    vector<string> operadores = {"+","-","/","*","!=" ,"==","=","<","<=","
 static    vector<string> palavrasReservadas = {"for", "if","while","E","OU","NAO"};
 static    vector<string> delimitadores = {" ","(",")",";"};
 
+static  Dicionario valores[26] = {
+        {"+","OPSOMA"},
+        {"-","OP SUB"},
+        {"*","OP MUL"},
+        {"/","OP DIV"},
+        {"!=","NAO IGUAL A"},
+        {"==","IGUAL"},
+        {"=","ATRIBUICAO"},
+        {"<","MENOR QUE"},
+        {">","MAIOR QUE"},
+        {"<=","MENOR OU IGUAL"},
+        {">=","MAIOR OU IGUAL"},
+        {"++","INCREMENTO"},
+        {"--","DECREMENTO"},
+        {";","FIM DA LINHA"},
+        {"(","ABRE PARENTESES"},
+        {")","FECHA PARENTESES"},
+        {"NUMR","NUMERO REAL"},
+        {"INT","INTEIRO"},
+        {"ID","IDENTIFICADOR"},
+        {"FOR","PALAVRA RESERVADA"},
+        {"IF","PALAVRA RESERVADA"},
+        {"WHILE","PALAVRA RESERVADA"},
+        {"<-","ATRIBUICAO"},
+        {"E","PALAVRA RESERVADA"},
+        {"OU","PALAVRA RESERVADA"},
+        {"NAO","PALAVRA RESERVADA"}
+    };
